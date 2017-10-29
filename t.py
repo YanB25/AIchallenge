6877b8c4fb7aaa18c24ef1c96f152e1ca899a34f
wyf <walkeryf@yeah.net>
2017-10-29 13:57:17 +0800

    add upload.sh and t.py

--- t.py
+++ t.py
@@ -1,6 +1,8 @@
 import config
+from readJson import *
 from PIL import Image as pim
-def getImageMatrixLabel(address, label, size = 1000, image_height = config.pic_height, image_width = config.pic_width):
+import numpy as np
+def getImageMatrixLabel(address, size = 1000, image_height = config.pic_height, image_width = config.pic_width):
     # return true_label 
     # config.debug_print('testing')
     # true_label = [[1 if k === label else 0 for k in range(config.Channel)] for j in label]
@@ -8,13 +10,15 @@ def getImageMatrixLabel(address, label, size = 1000, image_height = config.pic_h
     # image = readImage(address)
     # image_matrix = np.array(image);
     mats, labels = photo_address(address, size)
-    mats = [readImage(addr) for addr in mats]
+    mats = [readImage("{}/scene_train_images_20170904/{}".format(address, filename)) for filename in mats]
     return mats, labels
 
 def readImage(address):
-    image = pim.open(address).resize([config.pic_width,config.image_height])
+    image = np.array(pim.open(address).resize([config.pic_width,config.pic_height]))
     return image
 
 if __name__ == "__main__":
 #code below        
-    print( getImageMatrixLabel('../ai_challenger_scene_train_20170904')  )
+    a,b = getImageMatrixLabel('../ai_challenger_scene_train_20170904', 10)
+    print(a[0].shape)
+    print(b)
