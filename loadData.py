c4969e8182d43a337bf24b21eca066186ec72fa9
wyf <walkeryf@yeah.net>
2017-10-29 14:11:02 +0800

    rename

--- loadData.py
+++ loadData.py
@@ -2,7 +2,7 @@ import config
 from readJson import *
 from PIL import Image as pim
 import numpy as np
-def getImageMatrixLabel(address, size = 1000, image_height = config.pic_height, image_width = config.pic_width):
+def next_batch(address, size = 1000, image_height = config.pic_height, image_width = config.pic_width):
     mats, labels = photo_address(address, size)
     mats = [readImage("{}/scene_train_images_20170904/{}".format(address, filename)) for filename in mats]
     return mats, labels
@@ -13,6 +13,6 @@ def readImage(address):
 
 if __name__ == "__main__":
 #code below        
-    a,b = getImageMatrixLabel('../ai_challenger_scene_train_20170904')
+    a,b = next_batch('../ai_challenger_scene_train_20170904')
     print(a[0].shape)
     print(b)
